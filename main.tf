8bcb281d820f7297a408a2c02e2d4545a9b53702
Anton Podviaznikov <anton@podviaznikov.com>
2020-02-02 14:25:27 -0800

    https listener

--- main.tf
+++ main.tf
@@ -265,6 +265,24 @@ resource "aws_lb_listener" "listener" {
   protocol          = "HTTP"
   default_action {
     target_group_arn = aws_lb_target_group.lb_tg.arn
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https_listener" {
+   depends_on       = [aws_lb_target_group.lb_tg]
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
     type             = "forward"
+    target_group_arn = "${aws_lb_target_group.front_end.arn}"
   }
 }
