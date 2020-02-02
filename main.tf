f2a3e24b34b9648b87b58c0a03de388aebe75d73
Anton Podviaznikov <anton@podviaznikov.com>
2020-02-02 14:34:56 -0800

    open 443 port

--- main.tf
+++ main.tf
@@ -209,6 +209,12 @@ resource "aws_security_group" "lb_sg" {
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     protocol    = "-1"
     from_port   = 0
